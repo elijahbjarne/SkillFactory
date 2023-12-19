8791e609bff839c48c0cd7f8679ccc0b6c8cd569
Ilya Vladimirovich <67026530+elijahbjarne@users.noreply.github.com>
2023-12-19 13:41:50 +0300

    Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -2,12 +2,13 @@
 # Игра Крестики-нолики
 import os
 
-clear = lambda: os.system('cls')
+clear = lambda: os.system('cls') # Для красоты, что бы стирать прошлые выводы
 gui = [' ', '1', '2', '3',
         '1', '-', '-', '-',
         '2', '-', '-', '-',
         '3', '-', '-', '-']
 
+# Условия победы
 victory = [ [5, 6, 7],
             [9, 10, 11],
             [13, 14, 15],
@@ -17,7 +18,7 @@ victory = [ [5, 6, 7],
             [5, 10, 15],
             [7, 10, 13]]
 
-def print_gui():
+def print_gui(): # Вывод игры в консоль
     print(gui[0], end = " ")
     print(gui[1], end = " ")
     print(gui[2], end = " ")
@@ -38,7 +39,7 @@ def print_gui():
     print(gui[14], end = " ")
     print(gui[15])
 
-def step(column, line, symb):
+def step(column, line, symb): # Ставим символ в игру
     if column == 1 and line == 1:
         gui[5] = symb
     if column == 1 and line == 2:
@@ -60,7 +61,7 @@ def step(column, line, symb):
     if column == 3 and line == 3:
         gui[15] = symb
 
-def result():
+def result(): # Определяем победителя
     win = ""
     for i in victory:
         if gui[i[0]] == "X" and gui[i[1]] == "X" and gui[i[2]] == "X":
@@ -72,23 +73,24 @@ def result():
 game_over = False
 p1 = True
 
-while game_over == False:
+# Основная программа
+while game_over == False: 
     print_gui()
-    if p1 == True:
+    if p1 == True: # Ход первого игрока
         symb = "X"
         print("Игрок 1")
         column = int(input("Введите колонку: "))
         line = int(input("Введите строку: "))
-    else:
+    else: # Ход второго игрока
         symb = "O"
         print("Игрок 2")
         column = int(input("Введите колонку: "))
         line = int(input("Введите строку: "))
-    clear()
+    clear() # Убираем ненужное из консоли (для красоты)
     step(column, line, symb)
     win = result() 
-
-    if win != "":
+# Проверка конца игры
+    if win != "": 
         game_over = True
     else:
         game_over = False
